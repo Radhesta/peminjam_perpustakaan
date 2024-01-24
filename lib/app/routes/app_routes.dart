41b622501324bce8fde4f0a8b39dc5faaca833dc
Radhesta <radestaghifari@gmail.com>
2024-01-24 15:25:21 +0700

    first commit

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const REGISTER = _Paths.REGISTER;
+  static const BOOK = _Paths.BOOK;
+  static const PEMINJAMAN = _Paths.PEMINJAMAN;
+  static const LOGIN = _Paths.LOGIN;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const REGISTER = '/register';
+  static const BOOK = '/book';
+  static const PEMINJAMAN = '/peminjaman';
+  static const LOGIN = '/login';
+}
